a226b722d8ce416d8638dd33bf152d54ae220222
collinsbett24 <collinsbett24@gmail.com>
2022-07-09 02:58:24 +0300

    changes on users display with random me user api

--- src/app/services/api/sports-service.service.ts
+++ src/app/services/api/sports-service.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-
+import { catchError, map, Observable, of, retry } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { User } from 'src/app/models/user';
 @Injectable({
   providedIn: 'root'
 })
 export class SportsServiceService {
 
-  public ServerUrl = 'https://randomuser.me/api/?gender=female&nat=gb';
+  public ServerUrl = 'https://randomuser.me/api/';
 
   constructor(private http: HttpClient) { }
 
@@ -15,8 +16,34 @@ export class SportsServiceService {
     return this.http.get<any>(this.ServerUrl);
   }
 
+  //getTiles from diamond Surfaces
   getTiles(): Observable<any> {
     return this.http.get<any>(`http://nitco.liftwave.co.ke/api/getTile/Kitchen Tiles`);
-    // return this.http.get<any>(`http://nitco.liftwave.co.ke/api/getTile/${Term}`);
+  }
+
+
+  //get users from randommeUser Api
+  UsersApi(page: number, page_size: number) {
+    return ajax(`https://randomuser.me/api/?page=${page}&results=${page_size}&seed=abc`).pipe(
+      map((res: any) => {
+        if (res.response == null) {
+          console.log('Error Occured');
+          throw new Error('Value Expected');
+        }
+        let data = [];
+
+        for (let results in res.response) {
+          data.push(res.response[results])
+        }
+        return data;
+      }),
+      retry(1),
+      catchError(() => of([])));
+  }
+
+  filterUsersApi(page: number, page_size: number, gender: string, nationality: string) {
+    let nation = nationality.toLowerCase();
+    let gen = gender.toLowerCase();
+    return this.http.get<any>(`https://randomuser.me/api/?nat=${nation}&gender=${gen}&results=${page_size}`);
   }
 }
